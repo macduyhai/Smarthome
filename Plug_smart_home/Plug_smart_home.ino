cd8945ac54618dacab1277143ae62cc6335d05bf
haimd <haimd@beetsoft.com.vn>
2020-05-23 15:50:04 +0700

    Connect, reset mode wifi, control led build in from  Server MQTT

--- Plug_smart_home/Plug_smart_home.ino
+++ Plug_smart_home/Plug_smart_home.ino
@@ -1,152 +1,181 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>
+#include <PubSubClient.h>
 
-// Set web server port number to 80
-WiFiServer server(80);
+WiFiClient espClient;
+PubSubClient client(espClient);
+WiFiManager wifiManager;
 
-// Variable to store the HTTP request
-String header;
+#define ResetPin D5
 
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output4State = "off";
+bool MQTT_STT = false;
+// MQTT SERVER
 
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
+const char *mqtt_server = "broker.hivemq.com";
 
-void setup() {
-  Serial.begin(115200);
-  
-  // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(output4, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(output4, LOW);
+char *MAC = "";
+char *topic_in = "/v1/device/NTQ/"+MAC+"/telemetry" 
+char *topic_out = "/v1/device/NTQ/"+MAC+"/request/+"
 
-  // WiFiManager
-  // Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-  
-  // Uncomment and run it once, if you want to erase all the stored information
-  //wifiManager.resetSettings();
-  
-  // set custom ip for portal
-  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+//========================= FUnction ============================
 
-  // fetches ssid and pass from eeprom and tries to connect
-  // if it does not connect it starts an access point with the specified name
-  // here  "AutoConnectAP"
-  // and goes into a blocking loop awaiting configuration
-  wifiManager.autoConnect("PLUG_SMART_HOME");
-  // or use this for auto generated name ESP + ChipID
-  //wifiManager.autoConnect();
-  
-  // if you get here you have connected to the WiFi
-  Serial.println("Connected.");
-  
-  server.begin();
+
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  // Switch on the LED if an 1 was received as first character
+  Serial.print((char)payload[0]);
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
+    Serial.print("Turn on led\n");
+    // but actually the LED is on; this is because
+
+    // it is active low on the ESP-01)
+  }
+  else
+  {
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+    Serial.print("Turn off led\n");
+  }
 }
+//void mqttReconnect() {
+//    // reconnect code from PubSubClient example
+//}
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "clientId-jgJEbXcFr1";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    //    const char* id = "ESP";
+    //    const char* user = "xqdufvgb";
+    //    const char* passw = " 5RNJC91AjlAv";
+    if (client.connect(clientId.c_str()))
+    {
+      //if (client.connect(id, user, passw)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("TNQ_MQTT");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+// Check button reset
+int reset_stt_old = 1;
+int reset_stt_new = 1;
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
+
+void Check_reset() {
+  //  Serial.println("Check_reset");
+  reset_stt_new = digitalRead(ResetPin);
+  //  Serial.println(reset_stt_new);
+  if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+    Serial.println("//============== > RESET MODE < ============//");
+    MQTT_STT = false;
+
+    delay(1000);
+    if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+      delay(1000);
+      reset_stt_new = digitalRead(ResetPin);
+      Serial.println(reset_stt_new);
+      if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+        delay(1000);
+        reset_stt_new = digitalRead(ResetPin);
+        Serial.println(reset_stt_new);
+        if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+          delay(1000);
+          reset_stt_new = digitalRead(ResetPin);
+          Serial.println(reset_stt_new);
+          if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+            delay(1000);
+            reset_stt_new = digitalRead(ResetPin);
+            Serial.println(reset_stt_new);
+            if (reset_stt_new == 0 && reset_stt_old == 1 ) {
+              wifiManager.resetSettings();
+              reset_stt_old = reset_stt_new;
+              Serial.println("//==============< RESET DONE >============//");
+              ESP.restart();
             }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
         }
       }
+
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
+
   }
+}
+
+//// Cho den nhay chop  Them ham nay trong thu vien WiFiManager.h
+//unsigned long time1 = 0;
+//
+//void DisMode_Setup(){
+//  if ( (unsigned long) (millis() - time1) > 1000 )
+//    {
+//        if ( digitalRead(BUILTIN_LED) == LOW )
+//        {
+//            digitalWrite(BUILTIN_LED, HIGH);
+//        } else {
+//            digitalWrite(BUILTIN_LED, LOW );
+//        }
+//        time1 = millis();
+//    }
+//}
+void setup() {
+  Serial.begin(115200);
+  delay(5000);
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.println();
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(ResetPin, INPUT_PULLUP);
+
+  wifiManager.setTimeout(180);
+
+  if (!wifiManager.autoConnect("Plug_SmartHome_HAIMD")) {
+    MQTT_STT = false;
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  } else {
+    MQTT_STT = true;
+  }
+
+  Serial.println("=== > Connected ! Let's go :)");
+
+  client.setServer("broker.hivemq.com", 1883);
+  client.setCallback(mqttCallback);
+}
+
+void loop() {
+  if (MQTT_STT == true) {
+    if (!client.connected()) {
+      mqttReconnect();
+    }
+    client.loop();
+  }
+  Check_reset(); // Check button RESET
+  yield();
 }
